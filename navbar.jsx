de403133800709492fa50dddcc700867a892b764
syoceynv <anakbaik2and@gmail.com>
2026-08-18 11:51:05 +0700

    navbar.jsx

---
+++ navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Tugas Mapil</h2>
+
+      <div>
+        <a href="/">Home</a>
+        <a href="/akun">Akun</a>
+        <a href="/setting">Setting</a>
+      </div>
+    </nav>
+  );
+}
